9b3fdd52baca705c6b6a28ff842a52929907eeb3
DieZDied <diezdiedmari@gmail.com>
2025-01-21 19:02:01 +0700

    update

---
+++ js/order.js
@@ -0,0 +1,40 @@
+﻿let tg = window.Telegram.WebApp;
+let buy = document.getElementById('buy');
+let order = document.getElementById('order');
+tg.expand();
+
+buy.addEventListener('click', () => {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('form').style.display = 'block';
+    document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name;
+});
+
+order.addEventListener('click', () => {
+    document.getElementById('error').innerHTML = '';
+    let name = document.getElementById('user_name').value;
+    let email = document.getElementById('user_email').value;
+    let phone = document.getElementById('user_phone').value;
+
+    if (name === '' || email === '' || phone === '') {
+        document.getElementById('error').innerHTML = 'Please fill in all fields';
+        return;
+    }
+    if (email.length < 5 || email === "") {
+        document.getElementById('error').innerText = 'Please enter an real email address.';
+        return;
+    }
+    if (phone.length < 5) {
+        document.getElementById('error').innerText = 'Please enter an real phone number.';
+        return;
+    }
+
+    let data = {
+        name: name,
+        email: email,
+        phone: phone
+    }
+
+    tg.sendData(JSON.stringify(data));
+
+    tg.close();
+});
