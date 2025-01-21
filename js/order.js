ffcb1e4bafbcc507de955db4dae8f85432e46741
DieZDied <diezdiedmari@gmail.com>
2025-01-21 20:20:38 +0700

    update

---
+++ js/order.js
@@ -0,0 +1,42 @@
+﻿let tg = window.Telegram.WebApp;
+let buy = document.getElementById('buy');
+let order = document.getElementById('order');
+tg.expand();
+
+buy.addEventListener('click', () => {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('form').style.display = 'block';
+    document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name;
+});
+
+order.addEventListener('click', () => {
+    let error = document.getElementById('error');
+    error.innerText = "";
+    let name = document.getElementById('user_name').value;
+    let email = document.getElementById('user_email').value;
+    let phone = document.getElementById('user_phone').value;
+
+
+    if (name.length < 5) {
+        error.innerText = 'Please enter a valid name.';
+        return;
+    }
+    if (email.length < 5) {
+        error.innerText = 'Please enter an real email address';
+        return;
+    }
+    if (phone.length < 5) {
+        error.innerText = 'Please enter an real phone number';
+        return;
+    }
+
+    let data = {
+        name: name,
+        email: email,
+        phone: phone
+    }
+
+    tg.sendData(JSON.stringify(data));
+
+    tg.close();
+});
--- script.js
+++
@@ -1,8 +0,0 @@
-let h2 = document.querySelector("h2");
-
-let container = document.querySelector(".container");
-
-h2.onclick = function () {
-    container.classList.toggle("newyear");
-    this.classList.toggle("active");
-};
